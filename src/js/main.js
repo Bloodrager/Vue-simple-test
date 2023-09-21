61b238b71b10f7e7f5d2f678ff5d6e5faf7662d4
Bloodrager <rabvoc@gmail.com>
2023-09-21 16:59:04 +0200

    Commit simple vue test

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        newDude: "",
+        characters: [
+            "Mark Shade",
+            "Jane Parker",
+            "Stephan Smith"
+        ]
+    }
+})
